b31e979c28b8092307a44b3420f5c0de979e8ecb
javierpinacanas <pinacanas@gmail.com>
2025-03-03 22:17:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs26\fsmilli13333 \cf2 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 from flask import Flask, render_template, request, redirect, url_for, session\
+from flask import Flask, render_template, request, redirect, url_for, session\
 import os\
 import json\
 \
